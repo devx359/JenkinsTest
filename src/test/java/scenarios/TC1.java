3b0a0f146d703680e852079bf21f4da38c78fefe
debapriyo <debapriyo86@gmail.com>
2021-01-30 17:45:54 +0530

    headless option set 3

--- src/test/java/scenarios/TC1.java
+++ src/test/java/scenarios/TC1.java
@@ -11,8 +11,8 @@ public class TC1 {
 	@Test(priority=1)
 	public void setup()
 	{
-		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver_88.exe");
-		//System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver_88");
+		//System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver_88.exe");
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver_88");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("headless");
 		options.addArguments("window-size=1400,1500");
