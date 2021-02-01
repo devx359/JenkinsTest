b43b9687e811ef76e975ea4b0658871871a6147f
debapriyo <debapriyo86@gmail.com>
2021-02-01 17:52:20 +0530

    further changes

--- src/test/java/scenarios/TC1.java
+++ src/test/java/scenarios/TC1.java
@@ -13,6 +13,7 @@ public class TC1 {
 	{
 		//System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver_88.exe");
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver_88");
+		System.setProperty("webdriver.chrome.silentOutput", "true");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("headless");
 		options.addArguments("window-size=1400,1500");
